e7b8bd629c91bc2299fd00944736cbbe54686159
Asmenys <76791232+Asmenys@users.noreply.github.com>
2023-02-20 00:34:16 +0200

    styling changes, fixed the insert_branch method to work with adjanced_nodes as a map

--- bin/symbol_tree.cpp
+++ bin/symbol_tree.cpp
@@ -1,14 +1,17 @@
 #include "symbol_tree.hpp"
+#include "deque"
 #include "symbol_node.hpp"
-#include <set>
+#include <map>
 #include <string>
 
 SymbolTree::SymbolTree(std::string value) : root(SymbolNode(value)) {}
 
 void SymbolTree::insert_branch(std::deque<std::string> nodes) {
-  SymbolNode node = root;
+  SymbolNode *node = &root;
   while (!nodes.empty()) {
-    node = *node.adjacent_nodes.insert(SymbolNode(nodes.front())).first;
+    node->adjacent_nodes.insert(std::pair<std::string, SymbolNode>(
+        nodes.front(), SymbolNode(nodes.front())));
+    node = &node->adjacent_nodes.at(nodes.front());
     nodes.pop_front();
   }
 }
