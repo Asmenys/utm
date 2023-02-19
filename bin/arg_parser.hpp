cffba58b2dd775eed4ae9e14cd3712e3670ffb42
Asmenys <76791232+Asmenys@users.noreply.github.com>
2023-02-19 08:28:35 +0200

    moved options and definitions to private field

--- bin/arg_parser.hpp
+++ bin/arg_parser.hpp
@@ -6,9 +6,9 @@
 class ArgParser {
   static void help();
   static void start_machine(std::string filename);
+  static std::vector<char> options;
+  static std::vector<std::string> definitions;
 
 public:
   static void parse_args(int argc, char *argv[]);
-  static std::vector<char> options;
-  static std::vector<std::string> definitions;
 };
